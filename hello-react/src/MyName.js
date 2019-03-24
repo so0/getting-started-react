4e4bc7a4da3caea5530b405290eb29713dd5b503
so0 <syngshm@gmail.com>
2019-03-25 00:21:16 +0900

    함수형 컴포넌트, default props

--- hello-react/src/MyName.js
+++ hello-react/src/MyName.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 
-class MyName extends Component {
-    // props 의 기본값 설정
-    static defaultProps = {
-        name: '홍길동'
-    }
-    render() {
-        return (
-            <div>
-                안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다.
-            </div>
-        )
-    };
-}
+// 함수형 컴포넌트
+const MyName = ({ name }) => {
+    return (
+        <div>
+            안녕하세요! 제 이름은 <b>{name}</b> 입니다.
+        </div>
+    );
+};
+// class MyName extends Component {
+//     // props 의 기본값 설정
+//     static defaultProps = {
+//         name: '홍길동'
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다.
+//             </div>
+//         )
+//     };
+// }
 
 // 함수형 컴포넌트에서 defaultProps 설정 시
-// MyName.defaultProps = {
-//     name: '기본이름'
-// }
+MyName.defaultProps = {
+    name: '기본이름'
+}
 
 export default MyName;
